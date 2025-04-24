77a5bb9c4a3827f62b70413d3f527b287d3f2bac
danielbloy <daniel_bloy@hotmail.com>
2025-04-24 20:28:44 +0100

    Added memory module

--- cptkip/core/memory.py
+++ cptkip/core/memory.py
@@ -1,14 +1,22 @@
 # This module contains utility functions for examining how much RAM the
 # system has available whilst it is running.
-
 import gc
 
+import cptkip.core.environment as environment
 import cptkip.core.log as log
 
 
 def report_memory_usage(msg: str = ""):
     log.critical(f"MEMORY USAGE: {msg}")
-    log.critical(f"HEAP: Allocated: {gc.mem_alloc()} bytes, Free: {gc.mem_free()} bytes")
+    if environment.is_running_on_desktop():
+        import psutil as psutil
+        stats = psutil.virtual_memory()  # returns a named tuple
+        total_ram = stats.total / 1_073_741_824
+        free_ram = stats.free / 1_073_741_824
+        used_ram = stats.used / 1_073_741_824
+        log.critical(f"RAM: Used: {used_ram:.2f} GB, Free: {free_ram:.2f} GB, Total: {total_ram:.2f} GB")
+    else:
+        log.critical(f"HEAP: Allocated: {gc.mem_alloc()} bytes, Free: {gc.mem_free()} bytes")
 
 
 def report_memory_usage_and_free(msg: str = ""):
