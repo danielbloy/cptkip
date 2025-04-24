7c314d168001c05a366245513764452cb7f34e88
danielbloy <daniel_bloy@hotmail.com>
2025-04-24 07:54:43 +0100

    A bit more has been done

--- examples/cptkip/core/environment.py
+++ examples/cptkip/core/environment.py
@@ -5,5 +5,3 @@ print(f'Is running under test ............ : {environment.is_running_under_test(
 print(f'Is running on a microcontroller .. : {environment.is_running_on_microcontroller()}')
 print(f'Is running on a desktop .......... : {environment.is_running_on_desktop()}')
 print(f'Are pins available ............... : {environment.are_pins_available()}')
-print("")
-environment.report()
--- tests/CircuitPython/a_validate_core.py
+++ tests/CircuitPython/a_validate_core.py
@@ -1,5 +1,30 @@
-from cptkip.core.environment import is_running_on_microcontroller
-from cptkip.core.log import set_log_level, info, INFO
+def execute():
+    import cptkip.core.environment as environment
+    import cptkip.core.log as log
 
-set_log_level(INFO)
-info(f'Is running on a microcontroller: {is_running_on_microcontroller()}')
+    # Output some information about the environment we are executing in.
+    log.set_log_level(log.INFO)
+    log.info(f'Is running in CI ................. : {environment.is_running_in_ci()}')
+    log.info(f'Is running under test ............ : {environment.is_running_under_test()}')
+    log.info(f'Is running on a microcontroller .. : {environment.is_running_on_microcontroller()}')
+    log.info(f'Is running on a desktop .......... : {environment.is_running_on_desktop()}')
+    log.info(f'Are pins available ............... : {environment.are_pins_available()}')
+
+    # Set the log level to ERROR. None of the ERROR strings should be output.
+    log.set_log_level(log.ERROR)
+    log.critical('This text should appear')
+    log.error('This text should appear')
+    log.warn('ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR')
+    log.info('ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR')
+    log.debug('ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR')
+
+    log.set_log_level(log.INFO)
+    log.critical('This text should appear')
+    log.error('This text should appear')
+    log.warn('This text should appear')
+    log.info('This text should appear')
+    log.debug('ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR')
+
+
+if __name__ == '__main__':
+    execute()
