1270312247e0047d3ccffed75543236f6e69e149
danielbloy <daniel_bloy@hotmail.com>
2025-04-29 21:01:37 +0100

    A bit more

--- examples/d_blink_led.py
+++ examples/d_blink_led.py
@@ -1,81 +1,47 @@
+# This is designed to work with a Pimoroni Tiny 2040. If your board
+# differs then adjust for the appropriate pins.
+
 import time
 
-from interactive.animation import Flicker
-from interactive.button import ButtonController
-from interactive.environment import are_pins_available, is_running_on_microcontroller
-from interactive.led import Led
-from interactive.log import set_log_level, INFO, info
-from interactive.memory import report_memory_usage_and_free
-from interactive.polyfills.animation import BLACK, WHITE, AnimationSequence
-from interactive.polyfills.animation import Blink, Pulse
-from interactive.polyfills.button import new_button
-from interactive.polyfills.led import new_led_pin
-from interactive.polyfills.pixel import new_pixels
-from interactive.runner import Runner
-from interactive.scheduler import new_one_time_on_off_task
+import cptkip.core.environment as environment
+import cptkip.core.logging as log
 
-LED_YELLOW = None
-LED_GREEN = None
-LED_RED = None
+LED = None
 
-if are_pins_available():
+if environment.are_pins_available():
     # noinspection PyPackageRequirements
     import board
 
-    LED_YELLOW = board.GP6
-    LED_GREEN = board.GP5
-    LED_RED = board.GP1
+    LED = board.LED_R
 
-if __name__ == '__main__':
+log.set_log_level(INFO)
 
-    set_log_level(INFO)
+runner = Runner()
 
-    runner = Runner()
+led = Led(new_led_pin(LED))
 
-    yellow = Led(new_led_pin(LED_YELLOW))
-    green = Led(new_led_pin(LED_GREEN))
-    red = Led(new_led_pin(LED_RED))
-
-    red_animation = Blink(red, speed=0.5, color=WHITE)
-    green_animation = Pulse(green, speed=0.1, color=WHITE, period=2)
-
-    yellow_animations = [
-        Flicker(yellow, speed=0.1, color=WHITE),
-        Pulse(yellow, speed=0.1, color=WHITE, period=1),
-        Blink(yellow, speed=0.5, color=WHITE),
-    ]
-    yellow_animation = AnimationSequence(*yellow_animations, advance_interval=3)
+led_animation = Blink(led, speed=0.5, color=WHITE)
 
 
-    async def animate_leds() -> None:
-        if not runner.cancel:
-            if red_animation:
-                red_animation.animate()
-            if green_animation:
-                green_animation.animate()
-            if yellow_animation:
-                yellow_animation.animate()
+async def animate_leds() -> None:
+    if led_animation:
+        led_animation.animate()
 
 
-    runner.add_loop_task(animate_leds)
+runner.add_loop_task(animate_leds)
 
-    # Allow the application to only run for a defined number of seconds.
-    finish = time.monotonic() + 10
-
-
-    async def callback() -> None:
-        runner.cancel = time.monotonic() > finish
-        if runner.cancel:
-            yellow_animation.freeze()
-            green_animation.freeze()
-            red_animation.freeze()
-
-            yellow.off()
-            green.off()
-            red.off()
-            yellow.show()
-            green.show()
-            red.show()
+# Allow the application to only run for a defined number of seconds.
+finish = time.monotonic() + 10
 
 
-    runner.run(callback)
+async def callback() -> None:
+    runner.cancel = time.monotonic() > finish
+
+
+async def cleanup() -> None:
+    led_animation.freeze()
+    led.off()
+    led.show()
+
+
+runner.run(callback)
