540739eef2b9279d9eddb7c8035c9274a3e51837
danielbloy <daniel_bloy@hotmail.com>
2025-05-07 18:47:18 +0100

    Added some tests.

--- tests/cptkip/task/test_periodic_task.py
+++ tests/cptkip/task/test_periodic_task.py
@@ -19,11 +19,12 @@ def count_limiter(total: int) -> Callable[[], bool]:
     return func
 
 
+# noinspection PyTypeChecker
 class TestPeriodicTask:
 
     def test_using_default_values(self):
         """
-        Creates a task using all of the defaults (apart from the continue function)
+        Creates a task using all the defaults (apart from the continue function).
         """
         count: int = 0
 
@@ -49,3 +50,99 @@ class TestPeriodicTask:
 
         asyncio.run(task())
         assert count == 10
+
+    def test_frequency(self):
+        """
+        TODO
+        """
+        assert False
+
+    def test_using_begin_func(self):
+        """
+        Validates the begin function is called once and only once before the function is called.
+        """
+        count: int = 0
+
+        async def func() -> None:
+            assert begin_count == 1
+            nonlocal count
+            count += 1
+
+        begin_count: int = 0
+
+        async def begin_func() -> None:
+            assert count == 0
+            nonlocal begin_count
+            begin_count += 1
+
+        task = create(func, continue_func=count_limiter(10), begin_func=begin_func)
+
+        asyncio.run(task())
+        assert count == 10
+        assert begin_count == 1
+
+    def test_using_end_func(self):
+        """
+        Validates the end function is called once and only once after the function is called.
+        """
+        count: int = 0
+
+        async def func() -> None:
+            assert end_count == 0
+            nonlocal count
+            count += 1
+
+        end_count: int = 0
+
+        async def end_func() -> None:
+            assert count == 10
+            nonlocal end_count
+            end_count += 1
+
+        task = create(func, continue_func=count_limiter(10), end_func=end_func)
+
+        asyncio.run(task())
+        assert count == 10
+        assert end_count == 1
+
+    def test_using_begin_and_end_funcs(self):
+        """
+        Validates the begin and end functions are called once and only once, before and after
+        the function is called.
+        """
+        count: int = 0
+
+        async def func() -> None:
+            assert begin_count == 1
+            assert end_count == 0
+            nonlocal count
+            count += 1
+
+        begin_count: int = 0
+
+        async def begin_func() -> None:
+            assert count == 0
+            assert end_count == 0
+            nonlocal begin_count
+            begin_count += 1
+
+        end_count: int = 0
+
+        async def end_func() -> None:
+            assert begin_count == 1
+            assert count == 10
+            nonlocal end_count
+            end_count += 1
+
+        task = create(func, continue_func=count_limiter(10), begin_func=begin_func, end_func=end_func)
+
+        asyncio.run(task())
+        assert count == 10
+        assert begin_count == 1
+        assert end_count == 1
+
+    def test_with_initial_delay(self):
+        """
+        TODO
+        """
+        assert False
