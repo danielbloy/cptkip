264927cb3a0ba9e55aa81351e2136606c3e47b7c
danielbloy <daniel_bloy@hotmail.com>
2025-05-13 09:11:32 +0100

    Added initial button idea

---
+++ cptkip/device/button.py
@@ -0,0 +1,65 @@
+from adafruit_debouncer import Button
+
+import cptkip.core.environment as environment
+import cptkip.task.periodic_task as periodic_task
+
+# The timeframe to consider button presses to be a sequence for multi-clicks.
+BUTTON_SHORT_DURATION_MS = 200
+# The timeframe to consider a button being pressed should register as a long press.
+BUTTON_LONG_DURATION_MS = 2000
+# How often we poll the button
+BUTTON_FREQUENCY = 120
+
+# collections.abc is not available in CircuitPython.
+if environment.is_running_on_desktop():
+    from collections.abc import Callable, Awaitable
+
+
+# TODO: Should the handlers accept the button object as a parameter? Can we make it optional live PGZ
+
+def new(pin, click=None, multi_click=None, long_click=None,
+        continue_func=None, begin=None, end=None) -> Callable[[], Awaitable[None]]:
+    """
+    Returns a task that can be added to a runner.
+    TODO: Write comments
+
+    :param pin:
+    :param click:
+    :param multi_click:
+    :param long_click:
+    :param continue_func: If specified, this will be periodically called to confirm
+        the func should continue to be called.
+    :param begin: If specified, this will be executed once at the beginning
+        and before any initial delay.
+    :param end: If specified, this will be executed once at the end.
+    """
+    # TODO: This needs moving into hal as buttonpin. It'll likely make it simpler than making digital
+    #       pin support both input and output.
+    import digitalio
+    pin = digitalio.DigitalInOut(pin)
+    pin.direction = digitalio.Direction.INPUT
+    pin.pull = digitalio.Pull.UP
+
+    button = Button(pin, short_duration_ms=BUTTON_SHORT_DURATION_MS, long_duration_ms=BUTTON_LONG_DURATION_MS)
+
+    async def operation() -> None:
+        button.update()
+
+        short_count = button.short_count
+        if short_count != 0:
+
+            if short_count == 1 and click:
+                await click()
+
+            elif short_count > 1 and multi_click:
+                # TODO: Should multi-click be passed the number of clicks?
+                await multi_click()
+
+        if button.long_press and long_click is not None:
+            await long_click()
+
+    task = periodic_task.create(
+        operation, frequency=BUTTON_FREQUENCY, initial_delay=0,
+        continue_func=continue_func, begin=begin, end=end)
+
+    return task
