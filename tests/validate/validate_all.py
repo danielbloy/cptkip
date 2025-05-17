8f068336fe9c540bd6979d8f2b87696df3fab0fa
danielbloy <daniel_bloy@hotmail.com>
2025-05-17 21:45:06 +0100

    Minor refector

--- tests/validate/validate_all.py
+++ tests/validate/validate_all.py
@@ -3,10 +3,10 @@ import cptkip.core.memory as memory
 import tests.validate.validate_a_core as a
 import tests.validate.validate_b_config as b
 import tests.validate.validate_c_cpu as c
-import tests.validate.validate_d_tasks as d
+import tests.validate.validate_d_task as d
 import tests.validate.validate_e_pin as e
-import tests.validate.validate_f_devices as f
-import tests.validate.validate_g_animations as g
+import tests.validate.validate_f_device as f
+import tests.validate.validate_g_animation as g
 
 modules = [a, b, c, d, e, f, g]
 
