173311c6dcb174edc8ea65010b0bba39fbce2846
danielbloy <daniel_bloy@hotmail.com>
2025-05-26 20:52:32 +0100

    More renaming

--- tests/validate/validate_all.py
+++ tests/validate/validate_all.py
@@ -4,11 +4,12 @@ import tests.validate.validate_a_core as a
 import tests.validate.validate_b_config as b
 import tests.validate.validate_c_cpu as c
 import tests.validate.validate_d_task as d
+import tests.validate.validate_d_task_async as da
 import tests.validate.validate_e_pin as e
 import tests.validate.validate_f_device as f
 import tests.validate.validate_g_animation as g
 
-modules = [a, b, c, d, e, f, g]
+modules = [a, b, c, d, da, e, f, g]
 
 
 def execute():
