fd4581d6708854ce7fcd29ec9c1976ea92d35410
danielbloy <daniel_bloy@hotmail.com>
2025-05-26 21:15:49 +0100

    Added examples

--- examples/d_tasks_two_trivial_tasks.py
+++ examples/d_tasks_two_trivial_tasks.py
@@ -2,12 +2,11 @@
 # that the functions one and two will "drift" from the desired
 # frequencies whereas the period tasks example will not.
 
-import asyncio
 import time
 
 import cptkip.core.logging as log
 import cptkip.core.memory as memory
-import cptkip.task.basic_runner_async as runner
+import cptkip.task.basic_runner as runner
 
 memory.report_memory_usage()
 
@@ -16,17 +15,26 @@ log.set_log_level(log.INFO)
 # Run the loop for 5 seconds
 finish = time.monotonic() + 5
 
+next_one = 0
 
-async def one() -> None:
-    while time.monotonic() < finish:
+
+def one() -> bool:
+    global next_one
+    if time.monotonic() >= next_one:
         log.info(f"{time.monotonic()}: one")
-        await asyncio.sleep(0.3)
+        next_one = time.monotonic() + 0.3
+    return time.monotonic() < finish
 
 
-async def two() -> None:
-    while time.monotonic() < finish:
+next_two = 0
+
+
+def two() -> bool:
+    global next_two
+    if time.monotonic() >= next_two:
         log.info(f"{time.monotonic()}: two")
-        await asyncio.sleep(0.5)
+        next_two = time.monotonic() + 0.5
+    return time.monotonic() < finish
 
 
 runner.run([one, two])
