38182c346f0d98762e2e63bf37868fb35fb5f9ba
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-01 21:16:45 +0100

    Disable the examples from running.

--- cptkip/config/configuration.py
+++ cptkip/config/configuration.py
@@ -2,25 +2,11 @@
 # through settings in a config.py file which is located in the same place
 # as the working directory when the application is executed.
 import cptkip.core.logging as logging
-from cptkip.core.environment import is_running_on_desktop
 
 LOG_LEVEL = logging.WARNING
 
 # Try loading local device settings as overrides.
 try:
-
-    if is_running_on_desktop():
-        # noinspection PyUnusedImports
-        import importlib.util
-
-        config = importlib.import_module('config')
-        config = importlib.reload(config)
-    else:
-        # FUTURE: This could benefit from further investigation to see if we can
-        #         do something better. See the notes in import_driver().
-        config = __import__('config')
-
-    print(f"Config file {config.__file__} loaded.")
 
     # noinspection PyPackageRequirements
     from config import *
