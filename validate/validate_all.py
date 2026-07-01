d8f41e29bbea8d16e751b9a78ef472c059016661
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-01 08:02:39 +0100

    Minor structure change

--- validate/validate_all.py
+++ validate/validate_all.py
@@ -1,13 +1,13 @@
 import cptkip.core.logging as log
 import cptkip.core.memory as memory
-import tests.validate.validate_a_core as a
-import tests.validate.validate_b_config as b
-import tests.validate.validate_c_cpu as c
-import tests.validate.validate_d_task as d
-import tests.validate.validate_d_task_async as da
-import tests.validate.validate_e_pin as e
-import tests.validate.validate_f_device as f
-import tests.validate.validate_g_animation as g
+import validate.validate_a_core as a
+import validate.validate_b_config as b
+import validate.validate_c_cpu as c
+import validate.validate_d_task as d
+import validate.validate_d_task_async as da
+import validate.validate_e_pin as e
+import validate.validate_f_device as f
+import validate.validate_g_animation as g
 
 modules = [a, b, c, d, da, e, f, g]
 
