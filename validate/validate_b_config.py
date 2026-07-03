cd98c26e3a58af405b838b52e1cc490981a104b5
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-03 09:02:05 +0100

    Updated validate script

--- validate/validate_b_config.py
+++ validate/validate_b_config.py
@@ -3,21 +3,21 @@
 
 def execute():
     import cptkip.core.logging as log
-    log.set_log_level(log.INFO)
-    log.info('PASS')
+    log.set_log_level(log.DEBUG)
+    log.debug('PASS')
 
     import cptkip.config.configuration as config
 
     # This tests that the logging level is loaded from the config file along with other properties.
-    assert config.LOG_LEVEL == log.ERROR
+    assert config.LOG_LEVEL == log.INFO
     assert config.TEST_VALUE == 123.456
     assert config.TEST_STRING == "Hello world!"
     assert config.DEBUG
 
     log.critical('PASS')
     log.error('PASS')
-    log.warn('FAIL FAIL FAIL FAIL')
-    log.info('FAIL FAIL FAIL FAIL')
+    log.warn('PASS PASS PASS PASS')
+    log.info('PASS PASS PASS PASS')
     log.debug('FAIL FAIL FAIL FAIL')
 
 
