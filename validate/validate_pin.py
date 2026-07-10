c8f0f1466069dd38ed6d1a5a6b3d94cf7ddbdf32
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-10 21:57:36 +0100

    Added buzzer test and completed the validate_pins tests

--- validate/validate_pin.py
+++ validate/validate_pin.py
@@ -1,7 +1,10 @@
-# TODO: implement validation script
+import validate.pin.buzzer_pin as buzzer_pin
+import validate.pin.input_pin as input_pin
+import validate.pin.output_pin as output_pin
+import validate.pin.pwm_pin as pwm_pin
 import validate.utils as utils
 
-modules = []
+modules = [input_pin, output_pin, pwm_pin, buzzer_pin]
 
 if __name__ == '__main__':
     utils.execute_modules(modules)
