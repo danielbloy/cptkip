6cd329e6a94cf483a377d909eaee98155502e5ab
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-11 10:28:47 +0100

    Added in validation tests

--- validate/validate_task.py
+++ validate/validate_task.py
@@ -1,7 +1,8 @@
-# TODO: implement validation script
+import validate.task.basic_runner as basic_runner
+import validate.task.memory_monitor as memory_monitor
 import validate.utils as utils
 
-modules = []
+modules = [basic_runner, memory_monitor]
 
 if __name__ == '__main__':
     utils.execute_modules(modules)
