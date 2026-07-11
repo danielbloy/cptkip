d7c72b2da82a41cf1fa382ab7fcda3fb86e4cb64
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-11 13:14:17 +0100

    Added in async scripts toto validate task

--- validate/validate_task.py
+++ validate/validate_task.py
@@ -1,11 +1,12 @@
 import validate.task.basic_runner as basic_runner
+import validate.task.basic_runner_async as basic_runner_async
 import validate.task.memory_monitor as memory_monitor
+import validate.task.memory_monitor_async as memory_monitor_async
 import validate.task.periodic_task as periodic_task
+import validate.task.periodic_task_async as periodic_task_async
 import validate.utils as utils
 
-# TODO: Async tasks
-
-modules = [basic_runner, memory_monitor, periodic_task]
+modules = [basic_runner, memory_monitor, periodic_task, basic_runner_async, memory_monitor_async, periodic_task_async]
 
 if __name__ == '__main__':
     utils.execute_modules(modules)
