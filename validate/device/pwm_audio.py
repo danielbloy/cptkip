725e25d9fac05b0797bb8776952985d821bb4c31
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-15 18:15:28 +0100

    Added in placeholders validation scripts

---
+++ validate/device/pwm_audio.py
@@ -0,0 +1,13 @@
+# TODO: implement validation script
+
+def execute():
+    import validate.utils as utils
+
+    def task():
+        pass
+
+    utils.execute(task)
+
+
+if __name__ == '__main__':
+    execute()
--- validate/validate_device.py
+++ validate/validate_device.py
@@ -1,7 +1,12 @@
-# TODO: implement validation script
+import validate.device.button as button
+import validate.device.buzzer as buzzer
+import validate.device.led as led
+import validate.device.melody as melody
+import validate.device.pixels as pixels
+import validate.device.pwm_audio as pwm_audio
 import validate.utils as utils
 
-modules = []
+modules = [button, led, pixels, buzzer, melody, pwm_audio]
 
 if __name__ == '__main__':
     utils.execute_modules(modules)
