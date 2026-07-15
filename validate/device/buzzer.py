b4ffb5beae239e720aec11cf4c467e9bd76e92c3
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-15 18:35:59 +0100

    Add in more validation

--- validate/device/buzzer.py
+++ validate/device/buzzer.py
@@ -1,12 +1,34 @@
-# TODO: implement validation script
-
 def execute():
     import validate.utils as utils
 
-    def task():
-        pass
+    import cptkip.config.configuration as config
+    import cptkip.pin.buzzer_pin as buzzer_pin
+    from cptkip.device.buzzer import Buzzer
 
+    pin = buzzer_pin.BuzzerPin(config.BUZZER_PIN)
+    buzzer = Buzzer(pin)
+
+    pin.volume = 0.5
+    frequency = 500
+    buzzer.play(frequency, 0.25)
+
+    def task():
+        nonlocal frequency
+        buzzer.update()
+        if buzzer.playing:
+            return
+
+        frequency += 100
+        buzzer.play(buzzer.p, 0.25)
+
+    print("Buzzer will change frequency")
     utils.execute(task)
+
+    buzzer.off()
+    pin.deinit()
+
+    del buzzer
+    del pin
 
 
 if __name__ == '__main__':
