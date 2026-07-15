c8d37a83dba91d50690a202b819304a6f51686b4
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-15 20:59:54 +0100

    Further updates

--- validate/device/melody.py
+++ validate/device/melody.py
@@ -1,12 +1,31 @@
-# TODO: implement validation script
-
 def execute():
+    import cptkip.config.configuration as config
+    import cptkip.device.melody as melody
+    import cptkip.pin.buzzer_pin as buzzer_pin
     import validate.utils as utils
 
-    def task():
-        pass
+    pin = buzzer_pin.BuzzerPin(config.BUZZER_PIN)
+    pin.volume = 0.1
 
+    scale = '''C4:1 D:1 E:1 F:1 G:1 A:1 B:1 C5:1
+               B4:1 A:1 G:1 F:1 E:1 D:1 C:1'''
+
+    tune = [
+        "E:2", "E:2", "E:4", "E:2", "E:2", "E:4",
+        "E:2", "G:2", "C:2", "D:2", "E:8", "R:8"]
+
+    melody_sequence = melody.MelodySequence(
+        melody.Melody(pin, melody.decode_melody(scale.split()), 120),
+        melody.Melody(pin, melody.decode_melody(tune)))
+
+    def task():
+        melody_sequence.update()
+
+    print("Buzzer will play a tune")
     utils.execute(task)
+
+    pin.off()
+    del pin
 
 
 if __name__ == '__main__':
--- validate/device/pwm_audio.py
+++ validate/device/pwm_audio.py
@@ -1,12 +1,20 @@
-# TODO: implement validation script
-
 def execute():
+    import cptkip.config.configuration as config
+    import cptkip.device.pwm_audio as pwm_audio
     import validate.utils as utils
 
-    def task():
-        pass
+    audio = pwm_audio.Audio(config.BUZZER_PIN if config.BUZZER_PIN else "none")
+    queue = pwm_audio.Queue(audio)
+    queue.queue("validate/lion.mp3")
+    queue.queue("validate/lion.mp3")
 
+    def task():
+        queue.update()
+
+    print("Lion roar will play through buzzer")
     utils.execute(task)
+
+    audio.deinit()
 
 
 if __name__ == '__main__':
