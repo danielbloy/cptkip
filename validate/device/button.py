49bf9f69ef6e51ac851beab87837e256b2fb276f
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-15 18:19:17 +0100

    Added in button validation

--- validate/device/button.py
+++ validate/device/button.py
@@ -1,12 +1,37 @@
-# TODO: implement validation script
-
 def execute():
     import validate.utils as utils
 
-    def task():
-        pass
+    import cptkip.config.configuration as config
+    from cptkip.device.button import Button
+    import cptkip.pin.input_pin as inputpin
 
+    def single_click_handler() -> None:
+        print("Single click event")
+
+    def multi_click_handler() -> None:
+        print("Multi-click event")
+
+    def long_press_handler() -> None:
+        print("Long-press event")
+
+    input_pin = inputpin.InputPin(config.BUTTON_PIN, config.BUTTON_PULLUP)
+
+    button = Button(
+        input_pin,
+        click=single_click_handler,
+        multi_click=multi_click_handler,
+        long_click=long_press_handler)
+
+    def task():
+        button.update()
+
+    print("Press the button to test")
     utils.execute(task)
+
+    input_pin.deinit()
+    del input_pin
+
+    del button
 
 
 if __name__ == '__main__':
