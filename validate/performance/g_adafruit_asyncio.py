1e1b4fcb29067cc05c16aad428ca876a42bc785a
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-18 17:01:19 +0100

    Minor fix

--- validate/performance/g_adafruit_asyncio.py
+++ validate/performance/g_adafruit_asyncio.py
@@ -8,3 +8,9 @@ import asyncio
 print("BEFORE GC .. : Used:", gc.mem_alloc(), "bytes, Free:", gc.mem_free(), "bytes")
 gc.collect()
 print("AFTER GC ... : Used:", gc.mem_alloc(), "bytes, Free:", gc.mem_free(), "bytes")
+
+# Load the next file
+import supervisor
+
+supervisor.set_next_code_file("/validate/performance/z_finish.py")
+supervisor.reload()
