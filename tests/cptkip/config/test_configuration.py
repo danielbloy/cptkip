be3f179a3bd5b889528b10e4ab18520c4ec528c0
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-20 20:46:24 +0100

    Added tests

--- tests/cptkip/config/test_configuration.py
+++ tests/cptkip/config/test_configuration.py
@@ -3,15 +3,17 @@ import cptkip.core.logging as logging
 
 
 class TestNode:
+    # noinspection PyUnresolvedReferences
     def test_config_is_loaded(self) -> None:
         """
         Validates configuration defaults are loaded as well as the local overrides
-        contained in config.py.
+        contained in config.py and device.py.
         """
 
         # These are just random configuration values from the config.
-        # noinspection PyUnresolvedReferences
         assert configuration.TRIGGER_DISTANCE == 99999
         assert configuration.TEST_STRING == "Hello world!"
         assert configuration.TEST_VALUE == 123.456
         assert configuration.LOG_LEVEL == logging.WARNING
+        assert configuration.VALUE_TO_OVERRIDE_IN_DEVICE == 456
+        assert configuration.VALUE_ONLY_IN_DEVICE == "This is only in device.py"
