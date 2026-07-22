fdd034fdf0b8f8b1140bbcd477e298a1a71c4b94
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-22 21:43:38 +0100

    Added some more working scripts

--- validate/performance/n_pwm_audio.py
+++ validate/performance/n_pwm_audio.py
@@ -1,13 +1,26 @@
+import cptkip.config.configuration as config
+from cptkip.device.pwm_audio import Audio, Queue
 from validate.performance.task_runner import execute
+
+AUDIO_FILE = "examples/lion.mp3"
+
+audio = Audio(config.BUZZER_PIN)
+queue = Queue(audio)
 
 
 def task():
-    pass
-    # TODO: Implement
+    queue.update()
 
 
+queue.queue(AUDIO_FILE)
 execute(task, False)
+queue.cancel()
+
+queue.queue(AUDIO_FILE)
 execute(task, True)
+queue.cancel()
+
+audio.deinit()
 
 # Load the next file
 from validate.performance.script_runner import execute_next_script
