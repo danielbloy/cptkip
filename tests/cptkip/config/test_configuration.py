f179ee2d04acf8a51cb9a461cb3a63d8edd83628
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-25 15:14:17 +0100

    Updated config tests

--- tests/cptkip/config/test_configuration.py
+++ tests/cptkip/config/test_configuration.py
@@ -14,6 +14,8 @@ class TestNode:
         assert configuration.TRIGGER_DISTANCE == 99999
         assert configuration.TEST_STRING == "Hello world!"
         assert configuration.TEST_VALUE == 123.456
-        assert configuration.LOG_LEVEL == logging.WARNING
         assert configuration.VALUE_TO_OVERRIDE_IN_DEVICE == 456
         assert configuration.VALUE_ONLY_IN_DEVICE == "This is only in device.py"
+
+        # device.py overrides LOG_LEVEL to DEBUG (see tests/cptkip/config/device.py).
+        assert configuration.LOG_LEVEL == logging.DEBUG
