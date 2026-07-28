5fb92ba2c51019858f9b0fe7d7c2073b933909b2
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-28 21:30:51 +0100

    Added failing tests as placeholders

---
+++ validate/device/i2s_audio.py
@@ -0,0 +1,21 @@
+def execute():
+    import cptkip.config.configuration as config
+    from cptkip.device.audio import I2sAudio as Audio, Queue
+    import validate.utils as utils
+
+    audio = Audio(config.BUZZER_PIN if config.BUZZER_PIN else "none")  # TODO: Make work
+    queue = Queue(audio)
+    queue.queue("validate/lion.mp3")
+    queue.queue("validate/lion.mp3")
+
+    def task():
+        queue.update()
+
+    print("Lion roar will play through buzzer")
+    utils.execute(task)
+
+    audio.deinit()
+
+
+if __name__ == '__main__':
+    execute()
