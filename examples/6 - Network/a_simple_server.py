70161795cea26319f62d0a22fdc1753cab6fa9e2
Daniel Bloy <daniel_bloy@hotmail.com>
2026-07-31 18:11:52 +0100

    Working simple example server using biplane

---
+++ examples/6 - Network/a_simple_server.py
@@ -0,0 +1,37 @@
+import os
+import time
+
+import biplane
+import board
+import digitalio
+
+SSID = os.getenv('WIFI_SSID')
+PASSWORD = os.getenv('WIFI_PASSWORD')
+NAME = "app"
+
+server = biplane.Server()
+
+
+@server.route("/", "GET")
+def main(query_parameters, headers, body):
+    return biplane.Response("<b>Hello, world!</b>", content_type="text/html")
+
+
+def asyncio_sleep(
+        seconds):  # minimal implementation of asyncio.sleep() as a generator
+    start_time = time.monotonic()
+    while time.monotonic() - start_time < seconds:
+        yield
+
+
+def blink_builtin_led():
+    with digitalio.DigitalInOut(board.LED) as led:
+        led.switch_to_output(value=False)
+        while True:
+            led.value = not led.value
+            yield from asyncio_sleep(0.05)
+
+
+for _ in zip(blink_builtin_led(),
+             server.circuitpython_start_wifi_station(SSID, PASSWORD, NAME)):
+    pass
