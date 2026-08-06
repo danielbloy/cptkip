b3db5f45ecf209c4e52301966e4b8fdb0fcd0979
Daniel Bloy <daniel_bloy@hotmail.com>
2026-08-06 21:53:26 +0100

    Updated run.zero

--- cptkip/zero/run.py
+++ cptkip/zero/run.py
@@ -7,7 +7,19 @@ if environment.is_running_on_desktop():
     from collections.abc import Callable
 
 
-# TODO: Test
+def run_for(seconds: float | int, func: Callable[[], None]):
+    """
+    Runs the func for the given number of seconds.
+    """
+    if seconds <= 0:
+        raise ValueError(f"Argument {seconds} is not positive")
+    
+    finish = time.monotonic() + seconds
+
+    while time.monotonic() < finish:
+        func()
+
+
 def update_for(seconds: float | int, *args):
     """
     Runs update/animate on the arguments for the given number of seconds.
@@ -23,14 +35,3 @@ def update_for(seconds: float | int, *args):
                 raise ValueError(f"Argument {arg} is not callable")
 
     run_for(seconds, func)
-
-
-# TODO: Test
-def run_for(seconds: float | int, func: Callable[[], None]):
-    """
-    Runs the func for the given number of seconds.
-    """
-    finish = time.monotonic() + seconds
-
-    while time.monotonic() < finish:
-        func()
