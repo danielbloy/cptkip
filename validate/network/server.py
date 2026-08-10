be0ad0617e52a20e612cdbf6a1057ee02688e67f
Daniel Bloy <daniel_bloy@hotmail.com>
2026-08-10 07:04:05 +0100

    Initial skeleton for tests

---
+++ validate/network/server.py
@@ -0,0 +1,7 @@
+def execute():
+    # TODO: Implement
+    pass
+
+
+if __name__ == '__main__':
+    execute()
--- validate/validate_network.py
+++ validate/validate_network.py
@@ -1,1 +1,9 @@
-# TODO: Implement
+import validate.network.client as client
+import validate.network.server as server
+
+import validate.utils as utils
+
+modules = [client, server]
+
+if __name__ == '__main__':
+    utils.execute_modules(modules)
