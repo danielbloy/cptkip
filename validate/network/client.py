72339f4352df413f5df16db0f8397499e8121718
Daniel Bloy <daniel_bloy@hotmail.com>
2026-08-10 07:18:02 +0100

    Added client example

--- validate/network/client.py
+++ validate/network/client.py
@@ -1,6 +1,14 @@
 def execute():
-    # TODO: Implement
-    pass
+    from cptkip.network.requests import requests
+    import validate.utils as utils
+
+    def task():
+        with requests.request("GET", "http://www.adafruit.com/api/quotes.php") as response:
+            print(f"Status ..... : {response.status_code}")
+            print(f"Reason ..... : {response.reason}")
+            print(f"Text ....... : {response.json()[0]['text']}")
+
+    utils.execute(task)
 
 
 if __name__ == '__main__':
