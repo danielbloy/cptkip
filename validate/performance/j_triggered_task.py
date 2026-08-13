4aeafdd5c41367d5fb1bd42693451403700eb02c
Daniel Bloy <daniel_bloy@hotmail.com>
2026-08-13 22:26:03 +0100

    Added in final tests

--- validate/performance/j_triggered_task.py
+++ validate/performance/j_triggered_task.py
@@ -1,4 +1,19 @@
-# TODO: Implement
+from cptkip.task.triggered_task import create
+from validate.performance.task_runner import execute, continue_func
+
+
+def begin() -> None:
+    print("begin")
+
+
+def end() -> None:
+    print("end")
+
+
+task = create(lambda: True, 0.5, begin=begin, end=end, continue_func=continue_func)
+
+execute(task, False)
+execute(task, True)
 
 # Load the next file
 from validate.performance.script_runner import execute_next_script
