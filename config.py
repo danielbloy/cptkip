91604704980d0a0b374dfc80d1b938aaac96d92e
Daniel Bloy <daniel_bloy@hotmail.com>
2026-08-22 11:16:04 +0100

    Improved the configuration of well known properties

--- config.py
+++ config.py
@@ -23,18 +23,13 @@ if environment.are_pins_available():
     try:
         # noinspection PyUnresolvedReferences
         LED_PIN = board.LED
-        print('Using LED')
     except AttributeError:
         try:
             # noinspection PyUnresolvedReferences
             LED_PIN = board.LED_G
             LED_INVERT = True
-            print('Using LED_G')
         except AttributeError:
             pass
-
-if not LED_PIN:
-    print('No LED found')
 
 ################################################################################
 # B U T T O N
@@ -47,30 +42,12 @@ if environment.are_pins_available():
         # noinspection PyUnresolvedReferences
         BUTTON_PIN = board.BUTTON
         BUTTON_PULLUP = None
-        print('Using BUTTON')
     except AttributeError:
         try:
             # noinspection PyUnresolvedReferences
             BUTTON_PIN = board.GP27
-            print('Using GP27')
         except AttributeError:
             pass
-
-if not BUTTON_PIN:
-    print('No Button found')
-
-################################################################################
-# N E O P I X E L S
-################################################################################
-PIXELS_PIN = None
-PIXELS_COUNT = 8
-
-if environment.are_pins_available():
-    # noinspection PyUnresolvedReferences
-    PIXELS_PIN = board.GP28
-
-if not PIXELS_PIN:
-    print('No NeoPixels found')
 
 ################################################################################
 # B U Z Z E R
@@ -81,8 +58,15 @@ if environment.are_pins_available():
     # noinspection PyUnresolvedReferences
     BUZZER_PIN = board.GP3
 
-if not BUZZER_PIN:
-    print('No Buzzer found')
+################################################################################
+# N E O P I X E L S
+################################################################################
+PIXELS_PIN = None
+PIXELS_COUNT = 8
+
+if environment.are_pins_available():
+    # noinspection PyUnresolvedReferences
+    PIXELS_PIN = board.GP28
 
 ################################################################################
 # I 2 S    A U D I O
@@ -98,6 +82,3 @@ if environment.are_pins_available():
     I2S_LEFT_RIGHT_CLOCK = board.GP1
     # noinspection unresolved-references
     I2S_DATA = board.GP2
-
-if not I2S_BIT_CLOCK or not I2S_LEFT_RIGHT_CLOCK or not I2S_DATA:
-    print('No I2S Audio found')
